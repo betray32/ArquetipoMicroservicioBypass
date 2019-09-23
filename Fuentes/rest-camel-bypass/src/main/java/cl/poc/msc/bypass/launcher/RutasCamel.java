a5fc1f35d59de45f8affb828d9f08777108e213b
Camilo Contreras <contreras.camilo32@gmail.com>
2019-09-23 17:07:41 -0300

    Exponer servicio get

--- Fuentes/rest-camel-bypass/src/main/java/cl/poc/msc/bypass/launcher/RutasCamel.java
+++ Fuentes/rest-camel-bypass/src/main/java/cl/poc/msc/bypass/launcher/RutasCamel.java
@@ -13,16 +13,17 @@ import org.springframework.stereotype.Component;
 @Component
 public class RutasCamel extends RouteBuilder {
 
+	/**
+	 * Configuracion de camel
+	 */
 	@Override
 	public void configure() throws Exception {
 
-		rest().get("/hello")
-		.to("direct:hello");
-
-		from("direct:hello")
-		.log(LoggingLevel.INFO, "Logging :: route hello. returning Hello World")
-		.transform()
-		.simple("Hello World");
+		/*
+		 * Ejemplo de rutina exponiendo un servicio GET
+		 */
+		rest().get("/Get").to("direct:get");
+		from("direct:get").log(LoggingLevel.INFO, "Retornando un Hola mundo").transform().simple("Hello World");
 
 	}
 }
